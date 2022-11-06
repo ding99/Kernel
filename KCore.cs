5cb1a4e71e7ff7db538c48f07796ca1cb86aac4f
Wei Ding <ding_web@yahoo.co.jp>
2022-11-06 00:41:02 -0700

    update display

--- KCore.cs
+++ KCore.cs
@@ -2,12 +2,12 @@
     public class KCore {
 
         public double Add (double a, double b) {
-            Console.WriteLine($"- Addition for {a}, {b}");
+            Console.WriteLine($"-- Addition for {a}, {b}");
             return a + b;
         }
 
         public double Multiple(double a, double b) {
-            Console.WriteLine(($"- Multiplication for {a}, {b}"));
+            Console.WriteLine(($"-- Multiplication for {a}, {b}"));
             return a * b;
         }
 
