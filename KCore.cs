ba1a543cc8b67ddf9ccd291f07c29df253f41153
Wei Ding <ding_web@yahoo.co.jp>
2022-11-06 01:29:10 -0700

    simplize

--- KCore.cs
+++ KCore.cs
@@ -2,17 +2,14 @@
     public class KCore {
 
         public double Add (double a, double b) {
-            Console.WriteLine($"-- Addition for {a}, {b}");
             return a + b;
         }
 
         public double Subtract(double a, double b) {
-            Console.WriteLine(($"-- Subtraction for {a}, {b}"));
             return a - b;
         }
 
         public double Multiple(double a, double b) {
-            Console.WriteLine(($"-- Multiplication for {a}, {b}"));
             return a * b;
         }
 
