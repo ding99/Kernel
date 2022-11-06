d601cee9e545490002065bb3c4f4b2de49ff23ab
Wei Ding <ding_web@yahoo.co.jp>
2022-11-06 01:23:38 -0700

    substract

--- KCore.cs
+++ KCore.cs
@@ -6,6 +6,11 @@
             return a + b;
         }
 
+        public double Subtract(double a, double b) {
+            Console.WriteLine(($"-- Subtraction for {a}, {b}"));
+            return a - b;
+        }
+
         public double Multiple(double a, double b) {
             Console.WriteLine(($"-- Multiplication for {a}, {b}"));
             return a * b;
